6ef7b1b1c30f26566e2e7226c312b8c7dcbeb4e9
Nogginboink <github@companies.mucker.net>
2025-06-18 13:14:23 -0500

    added node setup

---
+++ node-setup/00-setup.sh
@@ -0,0 +1,17 @@
+#Update the OS
+apt-get update && sudo apt-get -y upgrade
+
+# Install necessary packages
+apt-get install -y apt-transport-https ca-certificates curl gpg
+
+# Disable swap
+sed -i '/swap.img/s/^#//g' /etc/fstab
+
+# Enable IPv4 forwarding
+sed -i '/ip_forward/s/^#//g' /etc/sysctl.conf
+
+# Enable br_netfilter
+echo "br_netfilter" > /etc/modules-load.d/br_netfilter.conf
+
+
+echo "Reboot the system and run 01-setup.sh to continue the setup."
---
+++ node-setup/01-setup.sh
@@ -0,0 +1,34 @@
+#Install containerd
+wget https://github.com/containerd/containerd/releases/download/v2.1.2/containerd-2.1.2-linux-amd64.tar.gz
+tar Cxzvf /usr/local containerd-2.1.2-linux-amd64.tar.gz
+https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+mv containerd.service /lib/systemd/system/
+systemctl daemon-reload
+systemctl enable --now containerd
+
+#Install runc
+wget https://github.com/opencontainers/runc/releases/download/v1.3.0/runc.amd64
+install -m 755 runc.amd64 /usr/local/sbin/runc
+
+#Install CNI plugins
+wget https://github.com/containernetworking/plugins/releases/download/v1.7.1/cni-plugins-linux-amd64-v1.7.1.tgz
+mkdir -p /opt/cni/bin
+tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.7.1.tgz
+
+#INSTALL kubeadm, kubelet, kubectl (v. 1.33)
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+#Configure cgroup driver to systemd
+
+#Create a cluster
+kubeadm init --config kubeadm-config.yaml
+
+#Make node1 a worker node
+kubectl label node kubernetesnode1 node-role.kubernetes.io/worker=worker
+
+#Install gateway CRDs
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml
